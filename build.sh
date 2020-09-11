147adf864df2fe46eaf0c32e53c6cd6e03753fb4
Mark Percival <m@mdp.im>
2020-09-11 03:32:30 +0000

    Preserve timestamps for s3

--- build.sh
+++ build.sh
@@ -4,8 +4,16 @@
 rm -rf out
 mkdir -p out
 
+mkdir -p archive
 
 npx next build && npx next export
+
+# Total mess, but s3 syncs based on timestamp.
+# Copy to from old archive, then back with the latest
+cp -p archive/*.mp3 out/.
+cp -p archive/*.txt out/.
+cp -p out/*.mp3 archive/.
+cp -p out/*.txt archive/.
 
 # Rename wpm.html for s3
 find ./out/ -type f ! -iname 'index.html' ! -name '404.html' -iname '*.html' -print0 | while read -d $'\0' f; do mv "$f" "${f%.html}"; done
