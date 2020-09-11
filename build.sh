65f7138603d1f7b98fff6a07afdbdde7048681f8
Mark Percival <m@mdp.im>
2020-09-11 03:19:48 +0000

    Work in progress

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 
 rm -rf out
 mkdir -p out
+
+
 npx next build && npx next export
+
+# Rename wpm.html for s3
+find ./out/ -type f ! -iname 'index.html' ! -name '404.html' -iname '*.html' -print0 | while read -d $'\0' f; do mv "$f" "${f%.html}"; done
 
 node podcast.js
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-find ./out/ -type f ! -iname 'index.html' ! -name '404.html' -iname '*.html' -print0 | while read -d $'\0' f; do mv "$f" "${f%.html}"; done
 docker run --rm -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli s3 sync --delete --acl public-read ./out s3://morse.mdp.im
 
