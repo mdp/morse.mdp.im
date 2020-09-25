8a1c0071e99db9ef1ed5ac596f3131cb66bd33cd
Mark Percival <m@mdp.im>
2020-09-25 01:49:06 +0000

    HTTPS

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 import './styles.css'
 
+function ensureHTTPS(window) {
+  if (window
+    && window.location.protocol === "http:"
+    && window.location.host === 'morse.mdp.im'
+  ) {
+    window.location.protocol = 'https'
+  }
+}
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    ensureHTTPS(window);
+  });
   return <Component {...pageProps} />
 }
