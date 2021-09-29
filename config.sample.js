4d4bae0f7bab1676f6fa783e16f5da1d1d9d0a5d
Mark Percival <m@mdp.im>
2021-09-29 19:43:27 +0000

    Usage tracking, no cookies and anonymous

---
+++ config.sample.js
@@ -0,0 +1,3 @@
+const config = {
+    TRACKING_ID: "GoogleAnalyticsHere"
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Head from 'next/head'
 import './styles.css'
+import { TRACKING_ID } from '../config'
 
 function ensureHTTPS(window) {
   if (window
@@ -11,12 +12,28 @@ function ensureHTTPS(window) {
   }
 }
 
+// Privacy preserving Google Analytics, no cookies and an anonymized IP
+// Only tracks pageviews for usage stats, no user or session tracking
+function anonymousGoogleTracking(window) {
+  if (window) {
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push(['js', new Date()]);
+    dataLayer.push(['config', TRACKING_ID, {client_storage: 'none', anonymize_ip: true}]);
+  }
+
+  const script = document.createElement('script');
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-6LX83HX0LX";
+  script.async = true;
+  document.body.appendChild(script);
+}
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
 
   React.useEffect(() => {
     ensureHTTPS(window);
-  });
+    anonymousGoogleTracking(window)
+  }, []);
 
   return (
     <>
